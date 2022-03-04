07946d22d0fd7a65d1dcc47933668a0923de596f
Nikita <you@example.com>
2022-03-04 12:20:50 +0500

    2

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,8 @@
     |                           | D2                        |      3        |
     |                           | D3                        |      2        |
     |     RDA5807               |                           |               |
-    |                           | SDIO (pin 8)              |     A4        |
-    |                           | SCLK (pin 7)              |     A5        |
+    |                           | SDA (pin 1)               |     A4        |
+    |                           | SCK (pin 2)               |     A5        |
     |     Buttons               |                           |               |
     |                           | Volume Up                 |               |
     |                           | Volume Down               |               |
@@ -27,11 +27,12 @@
 #include <RDA5807.h>
 #include <LiquidCrystal.h>
 
+#define DEBUG_INIT 1
+
 RDA5807 rx; 
 
 //unsigned int FM_STATION_FREQ = 10450; //104.50 Olimp
 unsigned int FM_STATION_FREQ = 10290; //102.90 Intervolna
-
 
 #define   CONTRAST_PIN   9
 #define   BACKLIGHT_PIN  7
@@ -45,15 +46,23 @@ uint8_t seekDirection = 1; // 0 = Down; 1 = Up. This value is set by the last en
 */
 
 //-------------------RDS-------------------
-#define DEBUG 0
-char buffer[30];
-unsigned int RDS[4];
-char seg_RDS[8];
-char seg_RDS1[64];
-char indexRDS1;
+/*
+#define POLLING_TIME  2000
+#define POLLING_RDS     80
 
-char hora,minuto,grupo,versio;
-unsigned long julian;
+char oldFreq[10];
+char oldStereo[10];
+char oldRssi[10];
+char oldRdsStatus[10];
+char oldRdsMsg[65];
+
+bool bSt = true;
+bool bRds = true;
+bool bShow = false;
+uint8_t seekDirection = 1; // 0 = Down; 1 = Up. This value is set by the last encoder direction.
+
+long pollin_elapsed = millis();
+*/
 //-----------------------------------------
 
 //LiquidCrystal lcd(12, 11, 5, 4, 3, 2, BACKLIGHT_PIN, POSITIVE );
@@ -93,6 +102,42 @@ byte frownie[8] = {
   0b10001
 };
 
+//----------------rx.getRdsProgramType()------------------
+// ---PTy код	Тип контента RDS
+//char имя[количество][длина]	
+char ProgramType [][22] = {"No type",	"News",
+                              "Current affairs",
+                              "Information",
+                              "Sport",
+                              "Education",
+                              "Drama",
+                              "Culture",
+                              "Science",
+                              "Varied",
+                              "Pop music",
+                              "Rock music",
+                              "Easy listening",
+                              "Light classical",
+                              "Serious classical",
+                              "Other music",
+                              "Weather",
+                              "Finance",
+                              "Childrens programmes",
+                              "Social affairs",
+                              "Religion",
+                              "Phone-in",
+                              "Travel",
+                              "Leisure",
+                              "Jazz music",
+                              "Country music",
+                              "National music",
+                              "Oldies music",
+                              "Folk music",
+                              "Documentary",
+                              "Alarm test",
+                              "Alarm"
+};
+
 /*
     Show some basic information on display
 */
@@ -129,113 +174,18 @@ void showStatus_on_lcd()
   //showRSSI();
 }
 
-// Show current frequency
-void showStatus()
-{
-  char aux[80];
-  sprintf(aux,"\nYou are tuned on %u MHz | RSSI: %3.3u dbUv | Vol: %2.2u | %s ",rx.getFrequency(), rx.getRssi(), rx.getVolume(), (rx.isStereo()) ? "Yes" : "No" );
-  Serial.println(aux);
-}
-
-//READ RDS  Direccion 0x11 for random access
-void ReadW()
-{
-   Wire.beginTransmission(0x11);            // Device 0x11 for random access
-   Wire.write(0x0C);                        // Start at Register 0x0C
-   Wire.endTransmission(0);                 // restart condition
-   Wire.requestFrom(0x11,8, 1);             // Retransmit device address with READ, followed by 8 bytes
-   for (int i=0; i<4; i++) {RDS[i]=256*Wire.read()+Wire.read();}        // Read Data into Array of Unsigned Ints
-   Wire.endTransmission();                  
-} 
-
-void get_RDS()
-{    
-  int i;
-  ReadW();      
-  grupo=(RDS[1]>>12)&0xf;
-      if(RDS[1]&0x0800) versio=1; else versio=0;  //Version A=0  Version B=1   
-      if(versio==0)
-      {
-       #if DEBUG             
-       sprintf(buffer,"Version=%d  Grupo=%02d ",versio,grupo); Serial.print(buffer);
-    //    Serial.print(" 0->");Serial.print(RDS[0],HEX);Serial.print(" 1->");Serial.print(RDS[1],HEX);Serial.print(" 2->");Serial.print(RDS[2],HEX);Serial.print(" 3->");Serial.println(RDS[03],HEX);
-    //    Serial.print(" 0->");Serial.print(RDS[0],BIN);Serial.print(" 1->");Serial.print(RDS[1],BIN);Serial.print(" 2->");Serial.print(RDS[2],BIN);Serial.print(" 3->");Serial.println(RDS[03],BIN);
-    #endif 
-    switch(grupo)
-    {
-     case 0:              
-      #if DEBUG 
-      Serial.print("_RDS0__");     
-      #endif
-      i=(RDS[1] & 3) <<1;
-      seg_RDS[i]=(RDS[3]>>8);       
-      seg_RDS[i+1]=(RDS[3]&0xFF);
-      //gotoXY(10,4);
-      for (i=0;i<8;i++)
-      {
-        #if DEBUG 
-        Serial.write(seg_RDS[i]);   
-        #endif
-        
-      }  
-      //Serial.print("FrecuAlt1-");Serial.println((RDS[2]>>8)+875);
-      //Serial.print("FrecuAlt2-"); Serial.println(RDS[2]&0xFF+875);      
-      
-      #if DEBUG                 
-      Serial.println("---");
-      #endif
-      break;
-     case 2:
-      i=(RDS[1] & 15) <<2;              
-      seg_RDS1[i]=(RDS[2]>>8);       
-      seg_RDS1[i+1]=(RDS[2]&0xFF);
-      seg_RDS1[i+2]=(RDS[3]>>8);       
-      seg_RDS1[i+3]=(RDS[3]&0xFF);
-      #if DEBUG 
-      Serial.println("_RADIOTEXTO_");
-              //Serial.print(i);Serial.print("   ");Serial.println(RDS[1] & 15);
-              //Serial.write(RDS[2]>>8); Serial.write (RDS[2]&0xFF);Serial.write(RDS[3]>>8);Serial.write(RDS[3]&0xFF);Serial.write("_");
-              for (i=0;i<32;i++)  Serial.write(seg_RDS1[i]);                                    
-              Serial.println("-TXT-");
-              #endif      
-              break;
-              case 4:             
-              i=RDS[3]& 0x003f;
-              minuto=(RDS[3]>>6)& 0x003f;
-              hora=(RDS[3]>>12)& 0x000f;
-              if(RDS[2]&1) hora+=16;
-              hora+=i;        
-              //z=RDS[2]>>1;
-              julian = RDS[2]>>1;
-              
-              if(RDS[1]&1) julian+=32768;
-              if(RDS[1]&2) julian+=65536;
-              #if DEBUG 
-              Serial.print("_DATE_");
-              Serial.print(" Juliano=");Serial.print(julian);
-              //sprintf(buffer," %02d:%02d ",hora,minuto); gotoXY(38,2);  LcdString(buffer); 
-              Serial.println(buffer); 
-              #endif            
-              break;
-              default:
-              #if DEBUG 
-              Serial.println("__"); 
-              #endif    
-              ;        
-            }                        
-    }                   
-}    
-      
-    
-   
 
 void setup() {
   //--------------------------------------------------------
   //-------------------------USART--------------------------
   //--------------------------------------------------------
   Serial.begin(9600);
+  if (DEBUG_INIT){
+    Serial.println("Serial.begin(9600); ------ OK");
+    delay(1000);
+  }
   //--------------------------------------------------------
-
+  
   //--------------------------------------------------------
   //--------------------------LCD---------------------------
   //--------------------------------------------------------
@@ -247,8 +197,12 @@ void setup() {
   lcd.print("Hello,   RDA5807");  
   lcd.setCursor ( 0, 1 );           // go to the next line
   lcd.print(" FORUM - fm     ");      
+  if (DEBUG_INIT){
+    Serial.println("lcd.begin(); --- DEBUG --- OK");
+    delay(1000);
+  }
   //--------------------------------------------------------
-  delay(100);
+ 
   //--------------------------------------------------------
   //-------------------------RDA5807------------------------
   //--------------------------------------------------------
@@ -258,62 +212,78 @@ void setup() {
   rx.setMono(true);                 // Force mono (value	TRUE = Mono; FALSE force stereo)
   // rx.setRBDS(true);              // set RDS and RBDS. See setRDS.
   rx.setRDS(true);                  // set RDS.
+  rx.setRBDS(true);                 // set the RBDS operation.
   rx.setRdsFifo(true);
+  rx.setFmDeemphasis(true);         //de	0 = 75 μs; 1 = 50 μs (75 μs. Used in USA (default); 50 μs. Used in Europe, Australia, Japan.)
   rx.setFrequency(FM_STATION_FREQ); // set STATION Freq.
+  
+  
+  if (DEBUG_INIT){
+    Serial.println("rx.setup(); RDA5807 ------ OK");
+    delay(1000);
+  }
   //--------------------------------------------------------
   
 }
 
 void loop() 
 {
-  delay(1000);
+  //delay(2000);
   //showStatus_on_lcd();
-  showStatus();
-  delay(1000);
-  Serial.println("---------------RDS -- get_RDS() in-------------------");
-  //get_RDS();
-  ReadW();
-  Serial.print("\n");
-  for (unsigned char i=0; i<64; i++)
-  {
-    
-    Serial.print(RDS[i], HEX);
-  }
-  Serial.print("\n");
-  Serial.println("---------------RDS -- get_RDS() out------------------");
-
-  Serial.println("---------------RDS -- TIME in-------------------");
-  Serial.print("\n");
-  Serial.println(RDS[0], BIN);
-  Serial.println(RDS[1], BIN);
-  Serial.println(RDS[2], BIN);
-  Serial.println(RDS[3], BIN);
-  Serial.println(RDS[4], BIN);
-  Serial.println(RDS[5], BIN);
-  Serial.println(RDS[6], BIN);
-  Serial.println(RDS[7], BIN);
-  Serial.print("\n");
-  Serial.println("---------------RDS -- TIME out-------------------");
-
-/*
-  if(rx.getRdsSync()){ //С этого момента RDS декодер синхронизирован. Данный факт отражается установкой флага RDSS регистра 0AH
-    Serial.println("С этого момента RDS декодер синхронизирован");
-    if(rx.getRdsReady()){
-      Serial.println("Информация получена");
-      Serial.print("rx.getRdsProgramType(): ");
-      Serial.println(rx.getRdsProgramType());
-      delay(3000);
-      Serial.print("rx.getRdsTime(): ");
-      Serial.println(rx.getRdsTime());
-      delay(3000);
-      Serial.print("rx.getRdsText(): ");
-      Serial.println(rx.getRdsText());
-      delay(3000);
-      Serial.print("rx.hasRdsInfo(): ");
-      Serial.println(rx.hasRdsInfo());
-      delay(3000);
+  //Serial.println("---------------RDS -- get_RDS() in-------------------");
+  
+  if(rx.getRdsSync()){    //С этого момента RDS декодер синхронизирован. Данный факт отражается установкой флага RDSS регистра 0AH
+    //Serial.println("С этого момента RDS декодер синхронизирован");
+    if(rx.getRdsReady()){ // "Информация получена" 
+      //Serial.println("Информация получена");
+      if( rx.hasRdsInfo()){  // Если достоверная доставерная (rx.hasRdsInfo()==true)    
+          //Serial.println("Информация достоверная");
+          
+          //
+          /*
+          Serial.print("rx.getRdsProgramType(): ");
+          for (unsigned char i=0; i<22; i++)
+          {
+            Serial.print(ProgramType [rx.getRdsProgramType()][i]);
+          }
+          */
+          Serial.println();
+          //Serial.print(ProgramType [rx.getRdsProgramType()][0]);
+          //Serial.println("RdsProgram: ");
+          //Serial.println(rx.getRdsProgramType());
+          
+          //Serial.print("rx.getRdsTime(): ");
+          //Serial.println(rx.getRdsTime());
+          //Serial.println("RdsText():  ");
+          /*
+          Serial.println(rx.getRdsText0A());
+          Serial.println(rx.getRdsText2A());
+          Serial.println(rx.getRdsText2B());
+          Serial.println(rx.getRdsTime());
+          Serial.print("ErrorBlockB: ");
+          Serial.println(rx.getErrorBlockB());
+          */
+          //Serial.print("rx.hasRdsInfo(): ");
+          //Serial.println(rx.hasRdsInfo());
+          //delay(1000);
+          Serial.println(rx.getRdsGroupType());
+          Serial.println(rx.getRdsVersionCode()); // 0 - A, 1- B
+          if(rx.getRdsVersionCode()){ //Если B (1- B)
+            Serial.print("RdsProgramType: ");
+            Serial.println(rx.getRdsProgramType());
+          }
+          Serial.println(rx.getRealFrequency()); //Возвращает реальную частоту приемника
+          Serial.println(rx.getRssi(), BIN); //RSSI; 000000 = min; 111111 = max; RSSI scale is logarithmic.
+          Serial.println(rx.getRssi(), DEC);
+          Serial.println(rx.getRdsText0A());
+          Serial.println(rx.getRdsText2A());
+          Serial.println(rx.getRdsText2B());
+          delay(1000);
+        }
+      //rx.clearRdsFifo();
     }
+
   }
-*/
+
 
 }
