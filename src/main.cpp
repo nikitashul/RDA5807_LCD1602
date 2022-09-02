473c5709b2e6585e6b58d63bf913a0cbcf531cfd
Nikita <shulyatjevnv@gmail.com>
2022-09-02 17:57:57 +0500

    Последняя версия добавил 3 кнопки и станции

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,10 @@
 /*
+ Надо глянуть проект
+ https://github.com/sdg12832/sdg12832/blob/master/Radio.ino#L88
+*/
+
+
+/*
     | Device name               | Device Pin / Description  |  Arduino Pin  |
     | ----------------          | --------------------      | ------------  |
     |     Display LCD1602       |                           |               |
@@ -8,24 +14,28 @@
     |                           | D1                        |      4        |
     |                           | D2                        |      3        |
     |                           | D3                        |      2        |
-    |     RDA5807               |                           |               |
+    |     RDA5807               | ------------------------- |               |
     |                           | SDA (pin 1)               |     A4        |
     |                           | SCK (pin 2)               |     A5        |
-    |     Buttons               |                           |               |
+    |     Buttons               | ------------------------- |               |
     |                           | Volume Up                 |               |
     |                           | Volume Down               |               |
     |                           | Stereo/Mono               |               |
     |                           | RDS ON/off                |               |
     |                           | SEEK (encoder button)     |               |
-    |     Encoder               |                           |               |
+    |     Encoder               | ------------------------- |               |
     |                           | A                         |               |
     |                           | B                         |               |
+    |     Button                | KEY 1   +                 |      7        | Подтяжка к +5
+    |     Button                | KEY 2   -                 |      8        | Подтяжка к +5
+    |     Button                | SET                       |      6        | Подтяжка к +5
 */
 
 #include <Wire.h>
 #include <Arduino.h>
 #include <RDA5807.h>
 #include <LiquidCrystal.h>
+#include <EEPROM.h>
 
 #define DEBUG_INIT 1 //Debug on/off
 
@@ -36,16 +46,35 @@
 #define D2 3
 #define D3 2
 
+#define KEY_1   7
+#define KEY_2   8
+#define KEY_SET 6
+
 RDA5807 rx; 
 
-//unsigned int FM_STATION_FREQ = 10450; //104.50 Olimp
-//unsigned int FM_STATION_FREQ = 10290; //102.90 Intervolna
-uint16_t FM_STATION_FREQ = 10290; //102.90 Intervolna
+uint16_t PROGRAMMS[] = {  9600,  // 96.00 ENERGY
+                          10000, //100.00 Радио 100
+                          10040, //100.40 Kontinental
+                          10080, //100.80 Bisness
+                          10160, //101.60 Europa+
+                          10200, //102.00 Novoe
+                          10240, //102.40 Studio
+                          10290, //102.90 Intervolna
+                          10350, //103.50 Nawe
+                          10450, //104.50 Olimp
+                          10490, //104.90 L-radio
+                          10680  //106.80 Iskatel
+                        
+                        };
+
+unsigned char SET_STATION = EEPROM.read(0); //Считываем какую станцию сохранили.
 
 //LiquidCrystal lcd(12, 11, 5, 4, 3, 2, BACKLIGHT_PIN, POSITIVE );
 LiquidCrystal lcd(RS, EN, D0, D1, D2, D3); //(rs, enable, d0, d1, d2, d3)
 
 // Creat a set of new characters
+
+
 byte smiley[8] = {
   0b00000,
   0b00000,
@@ -79,38 +108,38 @@ byte frownie[8] = {
   0b10001
 };
 
+
+
 /*
-    Show some basic information on display
+   Делаем "параллельное" выполнение нескольких задач
+   с разным периодом выполнения
 */
-void showStatus_on_lcd()
-{
+#define PERIOD_1 300    // период //Вывод данных на LCD1602
+#define PERIOD_2 10     // период //Период опроса кнопок
 
-  lcd.clear();
-  lcd.home ();                   // go to the home
-  lcd.print("CHANELL:        ");
-  lcd.setCursor ( 0, 1 );        // go to the next line
-  lcd.print(rx.getRealChannel());
-  delay(2000);
-  lcd.clear();
-  lcd.home ();                   // go to the home
-  lcd.print("REAL_FREQ:      ");
-  lcd.setCursor ( 0, 1 );        // go to the next line
-  lcd.print(rx.getRealFrequency());
-  delay(2000);
-  lcd.clear();
-  lcd.home ();                   // go to the home
-  lcd.print("RSSI:           ");
-  lcd.setCursor ( 0, 1 );        // go to the next line
-  lcd.print(rx.getRssi());
-  
-}
+unsigned long timer_1, timer_2; //Переменные для реализации таймеров
+uint8_t KEY_1_count = 0, KEY_2_count = 0, KEY_SET_count = 0;  //Счетчики для антидребезга
+//bool KEY_1_flag = 0, KEY_2_flag = 0, KEY_SET_flag = 0;  //Убрать неиспользуется
 
+void TIMER_TIC(); //Функция обрабатывает таймеры для выполнения задач.
+void showStatus_on_lcd(); //Функция обрабатывает вывод информации на LCD1602
+void BUTTON();  //Функция обрабатывает нажатия кнопок
 
 void setup() {
+  //Очистка памяти при первом программировании
+  //EEPROM.write(0, 0);
   //--------------------------------------------------------
   //-------------------------USART--------------------------
   //--------------------------------------------------------
   Serial.begin(9600);
+
+  pinMode(KEY_1, INPUT);       // устанавливает режим работы - вход
+  pinMode(KEY_2, INPUT);       // устанавливает режим работы - вход
+  pinMode(KEY_SET, INPUT);     // устанавливает режим работы - вход
+  digitalWrite(KEY_1, HIGH);   //Включаем подтяжку к +5В
+  digitalWrite(KEY_2, HIGH);   //Включаем подтяжку к +5В
+  digitalWrite(KEY_SET, HIGH); //Включаем подтяжку к +5В
+
   if (DEBUG_INIT){
     Serial.println("Serial.begin(9600); ------ OK");
     delay(1000);
@@ -121,15 +150,18 @@ void setup() {
   //--------------------------LCD---------------------------
   //--------------------------------------------------------
   lcd.begin(16,2);                  // initialize the lcd 
-  lcd.createChar (0, smiley);       // load character to the LCD
-  lcd.createChar (1, armsUp);       // load character to the LCD
-  lcd.createChar (2, frownie);      // load character to the LCD
-  lcd.home ();                      // go home
-  lcd.print("Hello,   RDA5807");  
-  lcd.setCursor ( 0, 1 );           // go to the next line
-  lcd.print(" -----TEST----- ");      
+      
   if (DEBUG_INIT){
-    Serial.println("lcd.begin(); --- DEBUG --- OK");
+    lcd.createChar (0, smiley);       // load character to the LCD
+    lcd.createChar (1, armsUp);       // load character to the LCD
+    lcd.createChar (2, frownie);      // load character to the LCD
+    delay(2000);
+    lcd.home ();                      // go home
+    lcd.print("Hello,   RDA5807");  
+    lcd.setCursor ( 0, 1 );           // go to the next line
+    lcd.print(" -----TEST----- ");    
+
+    Serial.println("lcd.begin();        ------ OK");
     delay(1000);
   }
   //--------------------------------------------------------
@@ -138,7 +170,7 @@ void setup() {
   //-------------------------RDA5807------------------------
   //--------------------------------------------------------
   rx.setup();
-  rx.setVolume(6);
+  rx.setVolume(4);
   rx.setBass(true);                 // Sets Bass Boost (value	FALSE = Disable; TRUE = Enable)
   rx.setMono(true);                 // Force mono (value	TRUE = Mono; FALSE force stereo)
   // rx.setRBDS(true);              // set RDS and RBDS. See setRDS.
@@ -146,18 +178,126 @@ void setup() {
   rx.setRBDS(false);                // set the RBDS operation.
   rx.setRdsFifo(false);
   rx.setFmDeemphasis(true);         //de	0 = 75 μs; 1 = 50 μs (75 μs. Used in USA (default); 50 μs. Used in Europe, Australia, Japan.)
-  rx.setFrequency(FM_STATION_FREQ); // set STATION Freq.
-  
+  rx.setFrequency(PROGRAMMS[SET_STATION]);    // set STATION Freq.
   
   if (DEBUG_INIT){
+    Serial.print("EEPROM.read(0);     ---- i= ");
+    Serial.println(SET_STATION);
+
     Serial.println("rx.setup(); RDA5807 ------ OK");
     delay(1000);
   }
   //--------------------------------------------------------
   
+  
 }
 
 void loop() 
 {
+  //Все действия обрабатываются по таймеру millis()
+  TIMER_TIC();
+}
+
+//Функция обрабатывает таймеры для выполнения задач.
+void TIMER_TIC()
+{
+  if (millis() - timer_1 > PERIOD_1) {    // условие таймера
+    timer_1 = millis();                   // сброс таймера
+    // выполняем блок №1 каждые PERIOD_1 миллисекунд
+    // Индикация в ФУНКЦИИ LCD 16*2
+    showStatus_on_lcd();
+  }  
+  if (millis() - timer_2 > PERIOD_2) {
+    timer_2 = millis();
+    // выполняем блок №2 каждые PERIOD_2 миллисекунд
+    //Обработка нажатия кнопок
+    BUTTON();
+    
+  }
+}
+
+//Функция обрабатывает нажатия кнопок
+void BUTTON(){
+
+  //---------------KEY_1---------------------
+  //Проверка, если нажата кнопка и не прошло 10 периодов то инкрементируем (10*10мсек)
+  if(digitalRead(KEY_1) == LOW && KEY_1_count<10){
+    KEY_1_count ++; 
+    if(KEY_1_count >= 10)
+    {
+      if(SET_STATION <= 10)
+      {
+        rx.setFrequency(PROGRAMMS[++SET_STATION]);   // set STATION Freq.
+      }
+    }
+  }
+  //Сброс состояния кнопки
+  if(digitalRead(KEY_1) == HIGH && KEY_1_count > 0)
+  {
+    KEY_1_count--;
+  }
+
+  //---------------KEY_2---------------------
+  //Проверка, если нажата кнопка и не прошло 10 периодов то инкрементируем (10*10мсек)
+  if(digitalRead(KEY_2) == LOW && KEY_2_count<10){
+    KEY_2_count ++; 
+    if(KEY_2_count >= 10)
+    {
+      if(SET_STATION > 0)
+      {
+        rx.setFrequency(PROGRAMMS[--SET_STATION]);   // set STATION Freq.
+      }
+    }
+  }
+  //Сброс состояния кнопки
+  if(digitalRead(KEY_2) == HIGH && KEY_2_count > 0)
+  {
+    KEY_2_count--;
+  }
+
+  //---------------KEY_SET---------------------
+  //Проверка, если нажата кнопка и не прошло 10 периодов то инкрементируем (10*10мсек)
+  if(digitalRead(KEY_SET) == LOW && KEY_SET_count<10){
+    KEY_SET_count ++; 
+    if(KEY_SET_count >= 10)
+    {
+      EEPROM.update(0, SET_STATION);
+      lcd.clear();
+      lcd.home ();                   // go to the home
+      lcd.print("EEPROM SET:        ");
+      lcd.setCursor ( 0, 1 );        // go to the next line
+      lcd.print(SET_STATION);
+      Serial.println("EEPROM SET: ");
+      Serial.print(SET_STATION);
+      delay(2000);
+    }
+  }
+  //Сброс состояния кнопки
+  if(digitalRead(KEY_SET) == HIGH && KEY_SET_count > 0)
+  {
+    KEY_SET_count--;
+  }
+  
+}
+
+//Функция обрабатывает вывод информации на LCD1602
+void showStatus_on_lcd()
+{
+  lcd.clear();
+  lcd.home ();                   // go to the home
+  lcd.print("FREQ: ");
+  lcd.print(rx.getRealFrequency());
+  lcd.setCursor ( 0, 1 );        // go to the next line
+  lcd.print("RSSI: ");
+  lcd.print(rx.getRssi());
+  if (DEBUG_INIT){
+    Serial.print("CHANELL: ");
+    Serial.println(rx.getRealChannel());
+    Serial.print("REAL_FREQ: ");
+    Serial.println(rx.getRealFrequency());
+    Serial.print("RSSI: ");
+    Serial.println(rx.getRssi());
+    
+  }
   
 }
